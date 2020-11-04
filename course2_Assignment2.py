b0d536b14f458a84be225eebb1bab1e47bbb5442
repl.it user <kmj990929@dgist.ac.kr>
2020-11-04 13:34:43 +0000

    course3

--- course2_Assignment2.py
+++ course2_Assignment2.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # Assignment 2
-# 
+
 # Before working on this assignment please read these instructions fully. In the submission area, you will notice that you can click the link to **Preview the Grading** for each step of the assignment. This is the criteria that will be used for peer grading. Please familiarize yourself with the criteria before beginning the assignment.
 # 
 # An NOAA dataset has been stored in the file `data/C2A2_data/BinnedCsvs_d400/fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89.csv`. This is the dataset to use for this assignment. Note: The data for this assignment comes from a subset of The National Centers for Environmental Information (NCEI) [Daily Global Historical Climatology Network](https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt) (GHCN-Daily). The GHCN-Daily is comprised of daily climate records from thousands of land surface stations across the globe.
